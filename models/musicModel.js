1c5cb715816e68fbe9276d0f886a78024f888969
amirhossein.mohammadi <amirhoseinmohammadisabet@gmail.com>
2022-09-24 06:59:11 +0330

    add delete and update api's part to music model

--- models/musicModel.js
+++ models/musicModel.js
@@ -1,45 +1,79 @@
-const { connectDB } = require('../config');
+const { Pool } = require('pg')
 
 async function findAll(req, res) {
-    const pool = connectDB();
-    const queryForGettingMusics = await pool.query('select * from musics')
-    const resultForGettingMusics = queryForGettingMusics.rows
+
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+    const queryForGetMusics = 'select * from music';
+    const resultForGetMusics = await (await pool.query(queryForGetMusics)).rows;
     pool.end();
-    return resultForGettingMusics;
+    return resultForGetMusics;
 }
 
 async function findById(id) {
-    const pool = connectDB();
-    const queryForGettingMusic = await pool.query(`SELECT * FROM musics where id=${id}`)
-    const resultForGettingMusic = queryForGettingMusic.rows;
+
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+    const queryForGetMusic = `SELECT * FROM music where id=${id}`;
+    const resultForGetMusic = await (await pool.query(queryForGetMusic)).rows;
     pool.end();
-    return resultForGettingMusic;
+    return resultForGetMusic;
 }
 
 async function create(obj) {
-    const pool = connectDB();
-    const queryForInsertingMusic = await pool.query(`insert into musics (singer, name) values ($1,$2)`, [obj.singer, obj.name])
-    return queryForInsertingMusic;
+
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+    let amir = await pool.query('insert into music (title ,singer) values ($1,$2)', [obj.title, obj.singer]);
+    return amir;
 }
 
 async function like(data) {
-    const pool = connectDB();
-    const queryChekForLike = await pool.query(`select * from likes where audience_id=${data.audience_id}`)
-    const resultCheckForLike = queryChekForLike.rows
 
-    if (resultCheckForLike.find(obj => obj.audience_id === data.audience_id && obj.music_id === data.music_id)) {
-        const queryForDeletingLikes = await pool.query(`DELETE FROM likes WHERE audience_id=${data.audience_id} AND music_id=${data.music_id}`)
-        const resultForDeletingLikes = queryForDeletingLikes.rows
-        console.log('remove');
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+    let checkForLike = await (await pool.query('select * from "like" where audience_id=($1)', [data.audience_id])).rows
+
+    const likeModel = {}
+    likeModel.audience_id = data.audience_id
+    likeModel.music_id = data.music_id
+
+    if (checkForLike.find(obj => obj.audience_id === likeModel.audience_id && obj.music_id === likeModel.music_id)) {
         pool.end();
-        return resultForDeletingLikes
+        console.log('liked before');
     }
     else {
-        const queryForLike = await pool.query(`insert into likes (audience_id, music_id) values ($1,$2)`, [data.audience_id, data.music_id])
-        const resultForLike = queryForLike.rows
-        console.log('insert');
+        const queryForLike = await (await pool.query('insert into "like" (audience_id, music_id) values ($1, $2)', [data.audience_id, data.music_id])).rows
         pool.end();
-        return resultForLike;
+        return queryForLike;
     }
 }
 
