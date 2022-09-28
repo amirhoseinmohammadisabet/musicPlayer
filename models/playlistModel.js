1b157c4e0a20f8ba23ce4b8f89033284412f29b8
amirhossein.mohammadi <amirhoseinmohammadisabet@gmail.com>
2022-09-28 06:29:02 +0330

    delete and update parts added to playlist model

--- models/playlistModel.js
+++ models/playlistModel.js
@@ -1,45 +1,80 @@
-const { connectDB } = require('../config');
+const { Pool } = require('pg')
 
 async function findAll(req, res) {
-    const pool = connectDB();
 
-    const queryGettingPlaylists = await pool.query('SELECT * FROM playlists')
-    const resultGettingPlaylists = queryGettingPlaylists.rows;
-    pool.end();
-    return resultGettingPlaylists;
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+    const queryGetPlaylists = `SELECT * FROM playlist`;
+    const resultGetPlaylists = await (await pool.query(queryGetPlaylists)).rows;
+    return resultGetPlaylists;
 }
 
 async function findById(id) {
-    const pool = connectDB();
 
-    const queryForGettingPlaylist = await pool.query(`SELECT * FROM playlists WHERE id=${id}`)
-    const resultForGettingPlaylist = queryForGettingPlaylist.rows
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+
+    let checkForLike = await (await pool.query('SELECT * FROM playlist where id=($1)', [id])).rows
     pool.end();
-    return resultForGettingPlaylist;
+    return checkForLike;
 }
 
 async function create(obj) {
-    const pool = connectDB();
 
-    const queryForInsertingPlaylist = await pool.query(`insert into playlists (name, audience_id) values ($1,$2)`, [obj.name, obj.audience_id])
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+    let checkForLike = await (await pool.query('insert into playlist ("name","user_id","info") values ($1,$2,$3)', [obj.name, obj.user_id, obj.info])).rows;
+
     pool.end();
-    return queryForInsertingPlaylist;
+    return checkForLike;
 }
 
-async function findSongs(playlistId) {
-    const pool = connectDB();
 
-    const queryForGettingMap = await pool.query(`SELECT * FROM "map_music_playlist" WHERE playlist_id=${playlistId}`)
-    const resultForGettingMap = queryForGettingMap.rows
+async function findSongs(obj) {
+
+    const pool = new Pool({
+        user: 'mirage',
+        host: '192.168.5.121',
+        Port: '5432',
+        database: 'college',
+        schema: 'mirage',
+        password: '&6Tw3C0V4q@w'
+    });
+
+
+    const query2 = `SELECT * FROM map_music_playlist where playlist_id=2`;
+    const res2 = await pool.query(query2);
     let mySong = []
-    for (let n = 0; n <= resultForGettingMap.length - 1; n++) {
-        const myMusic = `SELECT * FROM musics where id=${resultForGettingMap[n].music_id}`;
+    for (let n = 0; n <= res2.rows.length - 1; n++) {
+        const myMusic = `SELECT * FROM music where id=${res2.rows[n].music_id}`;
         const res3 = await pool.query(myMusic);
         mySong = mySong.concat(res3.rows)
     }
     pool.end();
     return mySong
 }
+
 
 module.exports = {
     findAll,
